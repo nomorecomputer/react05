1628ebef72fd101185e197f733b4a729822c810c
WeiLin <nomorecomputer@gmail.com>
2026-01-28 19:16:38 +0800

    fix cart.svg

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,18 @@ import process from "process";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // server: { open: true },
   base: process.env.NODE_ENV === "production" ? "/react05/" : "/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (/\.css$/.test(assetInfo.name)) {
+            return "assets/[name]-[hash][extname]";
+          }
+          return "assets/[name]-[hash][extname]";
+        },
+      },
+    },
+  },
 });
